0d83cd93cb5fe054493c11d8379f635265edd0a5
ZhengRanHui <1354709860@qq.com>
2019-03-05 12:27:06 +0800

    第二次提交

--- app/src/main/java/com/example/a13547/testgeihubjenkinsapplication/MainActivity.java
+++ app/src/main/java/com/example/a13547/testgeihubjenkinsapplication/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.a13547.testgeihubjenkinsapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView main_act_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main_act_tv = findViewById(R.id.main_act_tv);
+
+        main_act_tv.setText("second commit");
     }
 }
